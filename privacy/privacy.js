ab4d6c596b576d1b4fa6c88f95548721260e73c5
Liu-Xiao-Yan <xliu383@uottawa.ca>
2020-11-10 18:52:41 -0500

    Updated the page by a lot

--- privacy/privacy.js
+++ privacy/privacy.js
@@ -1,1 +1,45 @@
-
+function Displaystatistic1() {
+  var checkBox = document.getElementById("myCheck");
+  var text = document.getElementById("text");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+function Displaystatistic2() {
+  var checkBox = document.getElementById("myCheck2");
+  var text = document.getElementById("text2");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+function Displaystatistic3() {
+  var checkBox = document.getElementById("myCheck3");
+  var text = document.getElementById("text3");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+function Displaystatistic4() {
+  var checkBox = document.getElementById("myCheck4");
+  var text = document.getElementById("text4");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+function Displaystatistic5() {
+  var checkBox = document.getElementById("myCheck5");
+  var text = document.getElementById("text5");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
