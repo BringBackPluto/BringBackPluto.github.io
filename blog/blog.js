cbbb69f9acf5663a44041bb25eaf4e4cd9698f09
Tony Tam <ttam092@uottawa.ca>
2020-11-15 11:04:52 -0500

    added new nav bar

--- blog/blog.js
+++ blog/blog.js
@@ -2,3 +2,8 @@ function adjustHeight() {
     document.getElementById('pic').style.height = (document.getElementById('feature').offsetHeight - 19.5)+ "px";
 }
 
+function displayMenu(){
+    mainNav = document.getElementById('js-menu');
+    navBarToggle = document.getElementById('js-navbar-toggle');
+    mainNav.classList.toggle('active');
+}
