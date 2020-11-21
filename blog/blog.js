02cb0c112e86b5de3edaf1ae77594a333da41bf0
Tony Tam <ttam092@uottawa.ca>
2020-11-21 17:41:53 -0500

    added JS

--- blog/blog.js
+++ blog/blog.js
@@ -3,3 +3,11 @@ function displayMenu(){
     navBarToggle = document.getElementById('js-navbar-toggle');
     mainNav.classList.toggle('active');
 }
+function myFunction99() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
