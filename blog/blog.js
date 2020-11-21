24768f97e92f7aa611ab6633ef74f58db48763a0
Tony Tam <ttam092@uottawa.ca>
2020-11-21 16:10:44 -0500

    made it so theres no scroll bar on bottom of page

--- blog/blog.js
+++ blog/blog.js
@@ -1,7 +1,3 @@
-function adjustHeight() {
-    document.getElementById('pic').style.height = (document.getElementById('feature').offsetHeight - 19.5)+ "px";
-}
-
 function displayMenu(){
     mainNav = document.getElementById('js-menu');
     navBarToggle = document.getElementById('js-navbar-toggle');
